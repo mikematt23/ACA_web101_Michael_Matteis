0703ded91ef56e284ba8301c2f2d209d0087938b
Michael MAtteis <michaelcmatteis@gmail.com>
2020-02-27 18:14:31 -0600

    making layout and adding the fuction from class

---
+++ SRC/ticTacToe/tic-tac-toe.js
@@ -0,0 +1,27 @@
+var math = 1+10
+console.log('hey! Im working',math)
+
+//get an element from the dom
+var mainContainer = document.getElementsByClassName("mainContainer")
+console.log("mainContainer",mainContainer)
+  
+
+var previousPlay = null
+function addGamePiece (selectedElement) {
+// creating element
+var newElement = document.createElement('h1')
+
+previousPlay = setGamePiece()
+newElement.innerText = previousPlay
+selectedElement.appendChild(newElement)
+}
+function setGamePiece() {
+if (previousPlay === 'x') {
+return 'o'
+} else if (previousPlay === 'o') {
+return 'x'
+} else {
+return 'x'
+}
+}
+
