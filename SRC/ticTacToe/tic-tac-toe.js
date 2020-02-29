378e7490a1339009e3de178f73e98148b892b0cf
Michael MAtteis <michaelcmatteis@gmail.com>
2020-02-29 13:52:55 -0600

    making one move per square

--- SRC/ticTacToe/tic-tac-toe.js
+++ SRC/ticTacToe/tic-tac-toe.js
@@ -1,34 +1,29 @@
-var math = 1+10
-console.log('hey! Im working',math)
-
-//get an element from the dom
-var mainContainer = document.getElementsByClassName("mainContainer")
-console.log("mainContainer",mainContainer)
-  
-
 var previousPlay = null
-function addGamePiece (selectedElement) {
-// creating element
-var newElement = document.createElement('h1')
+function addGamePiece(selectedElement) {
+  // creating element
 
-previousPlay = setGamePiece()
-newElement.innerText = previousPlay
-selectedElement.appendChild(newElement)
+  if (selectedElement.children.length){
+    alert("Square has been played")
+  } else{
+    var newElement = document.createElement('h1')
+  }
+ 
+ 
+
+   previousPlay = setGamePiece()
+   newElement.innerText = previousPlay
+   selectedElement.appendChild(newElement)
 }
+
 function setGamePiece() {
-if (previousPlay === 'x') {
-return 'o'
-} else if (previousPlay === 'o') {
-return 'x'
-} else {
-return 'x'
-}
+  if (previousPlay === 'x') {
+    return 'o'
+  } else if (previousPlay === 'o') {
+    return 'x'
+  } else {
+    return 'x'
+  }
+ 
 }
 
-function onePiece(){
-  if(previousPlay === null){
-  return setGamePiece
-} else{
-  return null
-}
-}
+console.log(previousPlay)
