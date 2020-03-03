3f0589ec4c83545f4ab009d2ddbd16cf1a9e73ad
Michael MAtteis <michaelcmatteis@gmail.com>
2020-03-03 12:42:42 -0600

    making basic function of pieces

--- SRC/ticTacToe/tic-tac-toe.js
+++ SRC/ticTacToe/tic-tac-toe.js
@@ -2,23 +2,22 @@ var previousPlay = null
 function addGamePiece(selectedElement) {
   // creating element
 
+  checkEnd()
+
   if (selectedElement.children.length){
     alert("Square has been played")
   } else{
     var newElement = document.createElement('h1')
-  }
- 
- 
-
-   previousPlay = setGamePiece()
-   newElement.innerText = previousPlay
-   selectedElement.appendChild(newElement)
+    previousPlay = setGamePiece()
+    newElement.innerText = previousPlay
+    selectedElement.appendChild(newElement)
+   }
 }
 
 function setGamePiece() {
   if (previousPlay === 'x') {
     return 'o'
-  } else if (previousPlay === 'o') {
+  }  else if (previousPlay === 'o') {
     return 'x'
   } else {
     return 'x'
@@ -26,4 +25,8 @@ function setGamePiece() {
  
 }
 
-console.log(previousPlay)
+function checkEnd() {
+  
+}
+
+  console.log(previousPlay)
